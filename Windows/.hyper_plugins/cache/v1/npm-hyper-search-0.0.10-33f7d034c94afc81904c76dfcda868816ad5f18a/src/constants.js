4b9bba6d7b8d51cb2eb70ee10338c1b014caef79
Rahul Shah <rahuldshah3@gmail.com>
2018-07-01 15:23:26 -0400

    removed more unecessary stuff

--- Windows/.hyper_plugins/cache/v1/npm-hyper-search-0.0.10-33f7d034c94afc81904c76dfcda868816ad5f18a/src/constants.js
+++
@@ -1,6 +0,0 @@
-module.exports.LEAD_KEY = process.platform === 'darwin' ? 'Cmd' : 'Shift+Ctrl';
-module.exports.EDIT = 'Edit';
-module.exports.ENTER = 'Enter';
-module.exports.ESCAPE = 'Escape';
-module.exports.DIRECTION_NEXT = 1;
-module.exports.DIRECTION_PREV = -1;
